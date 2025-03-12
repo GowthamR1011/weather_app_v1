2a54521357448d01c8e208b03272ae7b544f8d7a
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-03-12 19:07:14 +0000

    Simplified the API response code.

--- src/app/api/city/[city]/route.ts
+++ src/app/api/city/[city]/route.ts
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
 import { WeatherData } from "@/interface/weatherdata";
 import { ErrorMessage } from "@/interface/errormessage";
+import { cookies } from "next/headers";
 
 type Context = {
    params: Promise<{
@@ -9,29 +10,24 @@ type Context = {
 }
 
 export async function GET(request:Request, context:Context ) {
-    const {params} = context;
-  
-  const {city} = await params;
+    
+    const {params} = context;  
+    const {city} = await params;
 
-const weatherdata:WeatherData | ErrorMessage  = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPENWEATHER_API_KEY}&q=${city}`,
-                                                {
-                                                    cache:"no-store"
-                                                })
-                            .then(async (res) => {
-                                const jsonData =  await res.json();
-                                if(jsonData.cod == 401){
-     
-                                    return {cod:500,message:"Unable to Fetch Data"};
-                                }
-                                if(jsonData.cod == 404)
-                                    return {cod:404, message:"City Not found"}
-                                return jsonData;
-                            }
-                            )
-                            .catch(err => {
-                                console.log(err)
-                               
-                            });
-        
-  return NextResponse.json({...weatherdata});
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?appid=${process.env.OPENWEATHER_API_KEY}&q=${city}`,
+    {
+        cache:"no-store"
+    })
+
+    const weatherdata:WeatherData | ErrorMessage = await response.json()
+
+    if(weatherdata.cod == 401)
+        return NextResponse.json({cod:500,message:"Unable to Fetch Data"});
+
+
+    if(weatherdata.cod == 500)
+        return NextResponse.json({cod:404,message:"City Not Found"});
+    
+    
+    return NextResponse.json({...weatherdata});
 }
