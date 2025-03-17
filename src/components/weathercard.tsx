60b8ef34d19de5a2ce7eb3eb3bad39533202f5dc
Gowtham Ravichander <gowtham.ravi1011@gmail.com>
2025-03-17 17:28:53 +0000

    made the grid responsive. Added a blue effect to the grid cards.
    Added various icons to the grid.
    
    Made the time depict according to the timezone of the city searched for.

--- src/components/weathercard.tsx
+++ src/components/weathercard.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image';
-
+import { FaTemperatureHigh, FaTemperatureLow, FaWind } from "react-icons/fa";
+import { WiHumidity } from "react-icons/wi";
+import { BsSunrise, BsSunset } from "react-icons/bs";
 
 import  { type WeatherData } from '@/interface/weatherdata';
 export interface IWeatherCardProps {
@@ -10,7 +12,7 @@ export interface IWeatherCardProps {
 
 export function WeatherCard ({weatherdata,changeMetrics,standardMetrics}: IWeatherCardProps ) {
   return (
-        <div className='bg-neutral-120 dark:bg-neutral-900  w-screen h-screen grid grid-cols-1 font-mono'> 
+        <div className='bg-white dark:bg-neutral-900 w-screen h-screen grid grid-cols-1 font-mono'> 
             <div className="flex-1 flex items-center justify-center m-auto">
                 <div className="grid ">
                 
@@ -43,28 +45,56 @@ export function WeatherCard ({weatherdata,changeMetrics,standardMetrics}: IWeath
 
             {/* More Details */}
             <div className='grid sm:grid-cols-3 grid-cols-2 content-evenly 
-                                 text-black bg-neutral-120
-                                 dark:text-white dark:bg-neutral-900
-                                  w-full gap-4'>
+                                 text-black bg-white/50
+                                 dark:text-white dark:bg-neutral-900/50
+                                 w-full gap-4 px-4 sm:px-8 md:px-16 py-4'>
 
-                    <div className='flex justify-center items-center text-xl p-4 gap-6 '>
-                        <>Max: {changeMetrics(weatherdata.main.temp_max)}{standardMetrics?<> °C</>:<> °F</>}</>
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <FaTemperatureHigh className="text-2xl" />
+                        <span className="text-sm">Max Temp</span>
+                        <span className="text-2xl">{changeMetrics(weatherdata.main.temp_max)}{standardMetrics?<> °C</>:<> °F</>}</span>
                     </div>
 
-                    <div className='flex justify-center items-center text-xl p-4 gap-6'>
-                        <>Min: {changeMetrics(weatherdata.main.temp_min)}{standardMetrics?<> °C</>:<> °F</>}</>
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <FaTemperatureLow className="text-2xl" />
+                        <span className="text-sm">Min Temp</span>
+                        <span className="text-2xl">{changeMetrics(weatherdata.main.temp_min)}{standardMetrics?<> °C</>:<> °F</>}</span>
                     </div>
-                    <div className='flex justify-center items-center text-xl p-4 gap-6 '>
-                        <>Humidity: {weatherdata.main.humidity}</>
+
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <WiHumidity className="text-3xl" />
+                        <span className="text-sm">Humidity</span>
+                        <span className="text-2xl">{weatherdata.main.humidity}%</span>
                     </div>
-                    <div className='flex justify-center items-center text-xl p-4 gap-6 '>
-                        <>Sunrise: {new Date(weatherdata.sys.sunrise * 1000).toLocaleTimeString([],{hour:"2-digit",minute:'2-digit'})}</>
+
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <BsSunrise className="text-2xl" />
+                        <span className="text-sm">Sunrise</span>
+                        <span className="text-2xl">
+                            {new Date((weatherdata.sys.sunrise + weatherdata.timezone) * 1000).toLocaleTimeString([], {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                hour12: true
+                            })}
+                        </span>
                     </div>
-                    <div className='flex justify-center items-center text-xl p-4 gap-6 '>
-                        <>Sunset: { new Date(weatherdata.sys.sunset * 1000).toLocaleTimeString([],{hour:"2-digit",minute:'2-digit'}) }</>
+
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <BsSunset className="text-2xl" />
+                        <span className="text-sm">Sunset</span>
+                        <span className="text-2xl">
+                            {new Date((weatherdata.sys.sunset + weatherdata.timezone) * 1000).toLocaleTimeString([], {
+                                hour: '2-digit',
+                                minute: '2-digit',
+                                hour12: true
+                            })}
+                        </span>
                     </div>
-                    <div className='flex justify-center items-center text-xl p-4 gap-6 '>
-                        <>Wind: {weatherdata.wind.speed}</>
+
+                    <div className='flex flex-col justify-center items-center text-xl p-4 gap-2 bg-slate-200/70 dark:bg-slate-600/80 rounded-lg backdrop-blur-md shadow-lg'>
+                        <FaWind className="text-2xl" />
+                        <span className="text-sm">Wind Speed</span>
+                        <span className="text-2xl">{weatherdata.wind.speed} m/s</span>
                     </div>
                 </div>
         
